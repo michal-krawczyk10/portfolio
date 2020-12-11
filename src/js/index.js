8ba68369947f6a5e7babe421e386f45762115620
Michał Krawczyk <michal.krawczyk10@gmail.com>
2020-12-11 23:06:32 +0100

    fb icon function added

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,30 @@
-import '../scss/main.scss';
+import "../scss/main.scss";
 
 // uncomment the lines below to enable PWA
-import {registerSW} from './pwa.js';
+import { registerSW } from "./pwa.js";
 registerSW();
 
 /* place your code below */
 
-console.log('HELLO 🚀')
+console.log("HELLO 🚀");
+
+const fblink = document.querySelector(".contact__links--fb--js");
+
+fblink.setAttribute(
+			"href",
+			"https://m.facebook.com/messages/compose?ids=pizmakkun"
+		);
+
+fblink.onclick = () => {
+	if (true) {
+		fblink.setAttribute(
+			"href",
+			"https://m.facebook.com/messages/compose?ids=pizmakkun"
+		);
+	} else {
+		fblink.setAttribute(
+	 		"href",
+			"https://m.facebook.com/messages/compose?ids=pizmakkun"
+		 );
+  }
+};
