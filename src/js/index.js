8815d3b7a31dc770579e87bda594f003ca52542d
Michał Krawczyk <michal.krawczyk10@gmail.com>
2020-12-12 22:58:10 +0100

    api inpro

--- src/js/index.js
+++ src/js/index.js
@@ -2,11 +2,19 @@ import "../scss/main.scss";
 import { registerSW } from "./pwa.js";
 registerSW();
 
-
 const fblink = document.querySelector(".contact__links--fb--js");
 
 fblink.onclick = () => {
-		fblink.setAttribute(
-			"href",
-			"https://www.facebook.com/pizmakkun");
+	fblink.setAttribute("href", "https://www.facebook.com/pizmakkun");
 };
+
+fetch("https://api.github.com/users/michal-krawczyk10/repos")
+	.then((resp) => resp.json())
+	.then((resp) => {
+		for (let repo of resp) {
+			console.log(repo);
+		}
+	})
+	.catch((error) => {
+		console.log(error);
+	});
